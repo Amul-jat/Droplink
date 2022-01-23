23e9bb130a71704f8da9d7161964b6f6c0529d74
Amul-jat <89834548+Amul-jat@users.noreply.github.com>
2022-01-23 12:22:24 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -111,8 +111,7 @@ async def multi_pdisk_up(ml_string):
         i += 1
 
     new_string = " ".join(new_ml_string)
-    #return await addFooter(new_string)
-    return addFooter(new_string)
+    return await addFooter(new_string)
 
 async def new_pdisk_url(urls):
     new_urls = []
