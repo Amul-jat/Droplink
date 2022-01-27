43f224567e6c8a9419c74356b19e12957cb38aa8
Amul-jat <89834548+Amul-jat@users.noreply.github.com>
2022-01-27 15:41:51 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -79,7 +79,7 @@ async def pdisk_up(link):
         async with aiohttp.ClientSession() as session:
             async with session.get(url, params=params, raise_for_status=True) as response:
                 data = await response.json()
-                v_url = """🔘__Episode__ - \nEn👉 """ + data["shortenedUrl"] + """\nHi👉Coming Soon......\n"""
+                v_url = """🔘__Episode__ - \nEn👉 """ + data["shortenedUrl"] + """\n"""
     else:
         v_url = link
         
