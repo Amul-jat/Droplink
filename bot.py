3424e522dc4d10959622285733da57914f2c6176
Amul-jat <89834548+Amul-jat@users.noreply.github.com>
2022-01-27 08:49:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -79,7 +79,7 @@ async def pdisk_up(link):
         async with aiohttp.ClientSession() as session:
             async with session.get(url, params=params, raise_for_status=True) as response:
                 data = await response.json()
-                v_url = """🔘__Episode__ - \nHi👉 """ + data["shortenedUrl"] + """\nEn👉\n\n"""
+                v_url = """🔘__Episode__ - \nEn👉 """ + data["shortenedUrl"]
     else:
         v_url = link
         
@@ -127,13 +127,13 @@ async def remove_username(new_List):
     return new_List
   
 async def addFooter(str):
-    footer = """\n\n__🔆Also available on Telegram in private channel Directly🔆__
+    footer = """\n__🔆Also available on Telegram in private channel Directly🔆__
 
-\n➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖
+➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖
 __#SavitaBhabhi #ComicVideo #Savita #Hindi #Sexy_voice #Kirtu #Savita_Bhabhi  #Crazydad #Momshelp #incest__
 
 🔅How to Download -""" + HOWTO + """\n
-📣 Provide By @"""+ CHANNEL + """\n
+📣 Provide By @"""+ CHANNEL + """
 __🔊For all  Direct Comics folder lifetime Membership msg me on @Kamdev07 or Join- @vip_comics__"""
     return str + footer
         
